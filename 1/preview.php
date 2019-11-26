b12e19ffa857455f6efe9e4c9ca2925e9c913a95
Влад <vlabster@mail.ru>
2019-11-26 19:46:08 +0300

    added article.php

--- 1/preview.php
+++ 1/preview.php
@@ -1,12 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
 <?php
 $articleName = $_REQUEST["articleName"];
 $articleText = $_REQUEST["articleText"];
-$articleLink = __DIR__ . "\acticle.php?articleName=$articleName";
+$articleLink = __DIR__ . "\acticle.php?articleName=$articleName&articleText=$articleText";
 if (strlen($articleText) > 199) {
     $article200Len = substr($articleText, 0, 199)."...";
     $count = 0;
     $ind = strlen($article200Len);
-    while ($count != 3) {
+    while ($count != 4) {
         if ($article200Len{$ind} == ' ') {
             $count++;
         }
@@ -14,6 +23,9 @@ if (strlen($articleText) > 199) {
     }
     $article200Len = substr($article200Len, 0, $ind)."<a href='$articleLink'>".substr($article200Len, $ind, 203)."</a>";
 }
-var_dump($articleLink);
-var_dump($article200Len);
+// var_dump($articleLink);
+// var_dump($article200Len);
+echo("<p>$article200Len</p>");
 ?>
+</body>
+</html>
