aed147a10ad2c22b2d4a03ed39aabfa41f5f175e
Влад <vlabster@mail.ru>
2019-11-26 20:48:26 +0300

    renamed $article200Len to $articlePreview

--- 1/preview.php
+++ 1/preview.php
@@ -12,20 +12,20 @@ $articleName = $_REQUEST["articleName"];
 $articleText = $_REQUEST["articleText"];
 $articleLink = "article.php?articleName=$articleName&articleText=$articleText";
 if (strlen($articleText) > 199) {
-    $article200Len = substr($articleText, 0, 199)."...";
+    $articlePreview = substr($articleText, 0, 199)."...";
     $count = 0;
-    $ind = strlen($article200Len);
+    $ind = strlen($articlePreview);
     while ($count != 4) {
-        if ($article200Len{$ind} == ' ') {
+        if ($articlePreview{$ind} == ' ') {
             $count++;
         }
         $ind--;
     }
-    $article200Len = substr($article200Len, 0, $ind)."<a href='$articleLink'>".substr($article200Len, $ind, 203)."</a>";
+    $articlePreview = substr($articlePreview, 0, $ind)."<a href='$articleLink'>".substr($articlePreview, $ind, 203)."</a>";
 }
 // var_dump($articleLink);
-// var_dump($article200Len);
-echo("<p>$article200Len</p>");
+// var_dump($articlePreview);
+echo("<p>$articlePreview</p>");
 ?>
 </body>
 </html>
